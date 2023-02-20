685f6d8d272d8bcff55501b7188f7b188b11bf9b
Rohan Borkhatariya <rohanborkhataria@gmail.com>
2023-02-20 17:59:06 +0530

    Mysql need string length for every string field so added it.

--- models/blocklist.py
+++ models/blocklist.py
@@ -5,4 +5,4 @@ class BlockListModel(db.Model):
     __tablename__ = "blocklist"
 
     id = db.Column(db.Integer, primary_key=True)
-    token = db.Column(db.String, unique=True, nullable=False)
+    token = db.Column(db.String(500), unique=True, nullable=False)
