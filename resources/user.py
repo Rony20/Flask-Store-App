62a4af938e7a9decd26a467b7c8ebe6472f6b9e4
Rohan Borkhatariya <rohanborkhataria@gmail.com>
2023-02-20 16:31:06 +0530

    Added mailgun to send the emails.

--- resources/user.py
+++ resources/user.py
@@ -1,33 +1,60 @@
+import os
+import requests
+
 from flask.views import MethodView
 from flask_smorest import abort, Blueprint
 from flask_jwt_extended import create_access_token, get_jwt
 from flask_jwt_extended import jwt_required
 from passlib.hash import pbkdf2_sha256
+from sqlalchemy import or_
 from sqlalchemy.exc import SQLAlchemyError
 
 from db import db
 from models import BlockListModel, UserModel
-from schemas import UserSchema
+from schemas import UserRegisterSchema, UserSchema
 
 
 blp = Blueprint("Users", __name__, description="Operations on users")
 
 
+def send_simple_message(to, subject, body):
+    domain = os.getenv("MAILGUN_DOMAIN")
+    api_key = os.getenv("MAILGUN_API_KEY")
+    return requests.post(
+        f"https://api.mailgun.net/v3/{domain}/messages",
+        auth=("api", api_key),
+        data={
+            "from": f"Rohan Borkhatariya <mailgun@{domain}>",
+            "to": [to],
+            "subject": subject,
+            "text": body
+        })
+
+
 @blp.route("/register")
 class UserRegister(MethodView):
-    @blp.arguments(UserSchema)
+    @blp.arguments(UserRegisterSchema)
     @blp.response(201)
     def post(self, user_data):
-        if UserModel.query.filter(UserModel.username == user_data["username"]).first():
-            abort(409, message="A user with that username already exists.")
+        if UserModel.query.filter(
+                or_(
+                    UserModel.username == user_data["username"],
+                    UserModel.email == user_data["email"]
+                )).first():
+            abort(409, message="A user with that username or email already exists.")
 
         user = UserModel(
             username=user_data["username"],
-            password=pbkdf2_sha256.hash(user_data["password"]))
+            password=pbkdf2_sha256.hash(user_data["password"]),
+            email=user_data["email"]
+        )
 
         try:
             db.session.add(user)
             db.session.commit()
+            send_simple_message(
+                user.email, "Successfully signed up",
+                f"Hi {user.username}! You have successfully signed up to the Stores REST API")
         except SQLAlchemyError:
             abort(500, message="An error occurred while creating an user")
 
